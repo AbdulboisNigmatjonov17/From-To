0700e21ad64801e5a9bb46a13cdc9e66354ff977
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-12-04 17:54:10 +0500

    nearly all pages finished

--- src/app/pages/context/context.ts
+++ src/app/pages/context/context.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { SelectModule } from 'primeng/select';
+import { Languages } from '../../models/Models';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TranslateService } from '../../services/translate/translate.service';
 
 @Component({
   selector: 'app-context',
-  imports: [],
+  imports: [SelectModule, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './context.html',
   styleUrl: './context.css',
 })
 export class Context {
+  languages: Languages[] | undefined;
+  text = new FormControl<string>('');
+  fromLanguages: Languages[] | undefined;
+  toLanguages: Languages[] | undefined;
 
+  selectedFromLanguage: string = 'uzn_Latn';
+  selectedToLanguage: string = 'eng_Latn';
+
+  constructor(private translateService: TranslateService,) {
+    this.getLangs();
+  }
+
+  getLangs() {
+    this.translateService.getLanguages().subscribe({
+      next: (languages: Languages[]) => {
+        this.fromLanguages = languages;
+        this.toLanguages = languages;
+      },
+      error: (err) => {
+        console.error("Error from server:", err);
+      }
+    });
+  }
+
+  swapLangs() {
+    const temp = this.selectedFromLanguage;
+    this.selectedFromLanguage = this.selectedToLanguage;
+    this.selectedToLanguage = temp;
+  }
+  clear() {
+    this.text.setValue('');
+  }
 }
--- src/app/pages/dictionary/dictionary.ts
+++ src/app/pages/dictionary/dictionary.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-dictionary',
-  imports: [],
+  imports: [FormsModule, SelectModule],
   templateUrl: './dictionary.html',
   styleUrl: './dictionary.css',
 })
-export class Dictionary {
+export class Dictionary implements OnInit {
+  countries: any[] | undefined;
 
+  selectedCountry: string | undefined;
+  ngOnInit() {
+    this.countries = [
+      { name: 'Australia', code: 'AU' },
+      { name: 'Brazil', code: 'BR' },
+      { name: 'China', code: 'CN' },
+      { name: 'Egypt', code: 'EG' },
+      { name: 'France', code: 'FR' },
+      { name: 'Germany', code: 'DE' },
+      { name: 'India', code: 'IN' },
+      { name: 'Japan', code: 'JP' },
+      { name: 'Spain', code: 'ES' },
+      { name: 'United States', code: 'US' }
+    ];
+  }
 }
