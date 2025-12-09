6361597daf41b2ead013011dcdecbc8fa1a9ef52
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-12-09 17:28:44 +0500

    registeration pages finish

--- src/app/pages/auth/sign-in/sign-in.ts
+++ src/app/pages/auth/sign-in/sign-in.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css',
 })
 export class SignIn {
-
+  seePassword: boolean = false;
+  togglePassword() {
+    this.seePassword = !this.seePassword;
+  }
 }
--- src/app/pages/auth/sign-up/sign-up.ts
+++ src/app/pages/auth/sign-up/sign-up.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css',
 })
+
 export class SignUp {
+  seePassword: boolean = false;
+  seeConfirm: boolean = false;
+
+  togglePassword() {
+    this.seePassword = !this.seePassword;
+  }
+
+  toggleConfirm() {
+    this.seeConfirm = !this.seeConfirm;
+  }
 
 }
